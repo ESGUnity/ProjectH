d3cee5bfd2e9b83c465dba07a6f365f3f1347303
[ESGUnity] <[sshsong3908@naver.com]>
2025-09-03 14:01:49 +0900

    feat : 카드 및 카드 효과

--- Assets/02_Script/Card/Card.cs
+++ Assets/02_Script/Card/Card.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class Card
 {
-    public int Id { get; set; }
-    public int Month { get; set; }
-
+    public int Id;
+    public int Month;
+    public bool IsGwang;
+    public bool IsGgeut;
+    public bool IsTti;
+    public bool IsSsangPi;
+    public CardEffect CE;
 }
